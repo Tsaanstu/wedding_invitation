55e7c080d24499c6c8ac03963395a5f9ab7b06e5
Vladimir Atasunts <atasunts.vladimir@yandex.ru>
2025-03-27 21:21:09 +0300

    add icons

--- frontend/src/components/Timeline.jsx
+++ frontend/src/components/Timeline.jsx
@@ -1,22 +1,46 @@
 import React from 'react';
 import '../styles.css';
 
-function Timeline() {
-    const events = [
-        {time: '14:00', title: 'Сбор гостей'},
-        {time: '15:00', title: 'Свадебная церемония'},
-        {time: '16:30', title: 'Праздничный банкет'},
-        {time: '22:00', title: 'Завершение вечера'},
-    ];
+const timelineData = [
+    {
+        time: '14:00',
+        title: 'Сбор гостей',
+        icon: '/icons/flower.svg',
+    },
+    {
+        time: '15:00',
+        title: 'Свадебная церемония',
+        icon: '/icons/rings.svg',
+    },
+    {
+        time: '16:30',
+        title: 'Праздничный банкет',
+        icon: '/icons/champagne.svg',
+    },
+    {
+        time: '18:00',
+        title: 'Подача торта',
+        icon: '/icons/cake.svg',
+    },
+    {
+        time: '22:00',
+        title: 'Завершение вечера',
+        icon: '/icons/fireworks.svg',
+    },
+];
 
+function Timeline() {
     return (
         <section className="section timeline" data-aos="fade-up">
             <h2>Программа дня</h2>
             <ul className="timeline-list">
-                {events.map((event, index) => (
-                    <li key={index} className="timeline-item">
-                        <span className="timeline-time">{event.time}</span>
-                        <span className="timeline-title">{event.title}</span>
+                {timelineData.map((item, index) => (
+                    <li className="timeline-item" key={index}>
+                        <div className="timeline-icon">
+                            <img src={item.icon} alt={item.title} />
+                        </div>
+                        <div className="timeline-time">{item.time}</div>
+                        <div className="timeline-title">{item.title}</div>
                     </li>
                 ))}
             </ul>
