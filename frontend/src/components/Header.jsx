c530f83abd8e37db8c59dce4e9a92c0998409789
Alina Aminova <kittexwisnocritix@gmail.com>
2025-04-07 23:17:49 +0300

    update sm

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,7 +5,6 @@ function Header() {
     return (
         <section className="section header" data-aos="fade-up">
             <h1 className="names">Владимир и Алина</h1>
-            <p className="date">16 августа 2025</p>
         </section>
     );
 }
