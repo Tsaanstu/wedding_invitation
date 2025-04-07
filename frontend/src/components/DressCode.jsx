ddbf06aa60a9b8311598baac05d92b52c174bf14
Alina Aminova <kittexwisnocritix@gmail.com>
2025-04-08 00:05:17 +0300

    fix style code

--- frontend/src/components/DressCode.jsx
+++ frontend/src/components/DressCode.jsx
@@ -5,27 +5,34 @@ function DressCode() {
     return (
         <section className="section dress-code" data-aos="fade-up">
             <h2>Дресс-код</h2>
-            <div className="dress-code-description" style={{ width: '600px'}}>
+            <div className="dress-code-description" style={{width: '600px'}}>
                 Для нас очень важна обстановка, в которой пройдет наше торжество,
                 поэтому мы выбрали красивое место и наняли команду професcионалов для его оформления.
-                Чтобы поддержать гармонию нашего мероприятия, мы настоятельно просим наших гостей соблюдать строгий стиль и дресс-код.
+                Чтобы поддержать гармонию нашего мероприятия, мы настоятельно просим наших гостей соблюдать строгий
+                стиль и дресс-код.
             </div>
+
             <header className="dress-code-style-header">
                 <h3>Стиль</h3>
             </header>
-            <div className="dress-code-style" style={{ width: '500px'}}>
+
+            <div className="dress-code-style" style={{width: '500px'}}>
                 Пожалуйста, при выборе нарядов придерживайтесь классического стиля.
-                Предпочтите спортивному костюму рубашку и джинсы, воздержитесь от слишком коротких нарядов и глубоких декольте.
+                Предпочтите спортивному костюму рубашку и джинсы, воздержитесь от слишком коротких нарядов и глубоких
+                декольте.
                 Возможно, в день мероприятия будет жарко, поэтому допускаются шорты классического кроя.
             </div>
+
             <header className="dress-code-palette-header">
                 <h3>Цветовая гамма</h3>
+
             </header>
-            <div className="dress-code-palette" style={{ width: '500px'}}>
+
+            <div className="dress-code-palette" style={{width: '500px'}}>
                 Ниже представлены цвета, которые отлично подойдут к нашему мероприятию.
                 <p>P.S. Пожалуйста, не допускайте белых и черных элементов одежды в вашем наряде (даже мужчины).</p>
+
             </div>
-            <p></p>
 
             <div className="color-palette">
                 <svg width="260" height="260" viewBox="0 0 260 260" xmlns="http://www.w3.org/2000/svg">
@@ -44,15 +51,15 @@ function DressCode() {
                     </g>
 
                     <g transform="translate(5, 90)">
-                        <circle cx="40"  cy="40" r="40" fill="#BBDEFB" />
-                        <circle cx="125" cy="40" r="40" fill="#90CAF9" />
-                        <circle cx="210" cy="40" r="40" fill="#468FAF" />
+                        <circle cx="40" cy="40" r="40" fill="#BBDEFB"/>
+                        <circle cx="125" cy="40" r="40" fill="#90CAF9"/>
+                        <circle cx="210" cy="40" r="40" fill="#468FAF"/>
                     </g>
 
                     <g transform="translate(5, 175)">
-                        <circle cx="40"  cy="40" r="40" fill="#DDB892" />
-                        <circle cx="125"  cy="40" r="40" fill="#B08968" />
-                        <circle cx="210" cy="40" r="40" fill="#9C6644" />
+                        <circle cx="40" cy="40" r="40" fill="#DDB892"/>
+                        <circle cx="125" cy="40" r="40" fill="#B08968"/>
+                        <circle cx="210" cy="40" r="40" fill="#9C6644"/>
                     </g>
 
                 </svg>
